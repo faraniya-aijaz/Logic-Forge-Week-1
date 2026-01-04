1e80f137d6b6ce3670f5c19f1a2ced601abb7b50
faraniya-aijaz <faraniyaaijaz8@gmail.com>
2026-01-05 04:56:07 +0500

    challenge_1.py

--- challenge_1.py
+++ challenge_1.py
@@ -1,17 +1,30 @@
-def product_except_self(contributions):
+# FIRST TRY
+# def contributions(arr):
+#     result = []
+#     for i in range(len(arr)):
+#         prod = 1
+#         for j in range(len(arr)):
+#             if i != j:
+#                 prod *= arr[j]
+#         result.append(prod)
+#     return result
+# arr1 = [1, 2, 3, 4]
+# arr2 = [-1, 1, 0, -3, 3]
+
+# print(contributions(arr1)) 
+# print(contributions(arr2)) 
+
+# SECOND
+def Team_Contributions(contributions):
     n = len(contributions)
     impact = [1] * n
-
-    # Left product
     for i in range(1, n):
         impact[i] = impact[i - 1] * contributions[i - 1]
-
-    # Right product
     right = 1
     for i in range(n - 1, -1, -1):
         impact[i] = impact[i] * right
         right = right * contributions[i]
 
     return impact
-print(product_except_self([1, 2, 3, 4]))
-print(product_except_self([-1, 1, 0, -3, 3]))
+print(Team_Contributions([1, 2, 3, 4]))
+print(Team_Contributions([-1, 1, 0, -3, 3]))
