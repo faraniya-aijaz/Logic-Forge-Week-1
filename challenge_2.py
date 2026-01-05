c0118d5bd60f768ca5f60f1459a731ea7749c9d4
faraniya-aijaz <faraniyaaijaz8@gmail.com>
2026-01-05 05:12:14 +0500

    Challenge_2.py

--- challenge_2.py
+++ challenge_2.py
@@ -1,3 +1,75 @@
+#first try
+# def min_window(log, pattern):
+#     ans = ""
+#     min_len = 10**9  
+
+#     for i in range(len(log)):
+#         for j in range(i, len(log)):
+#             sub = log[i:j+1]
+
+#             need = {}
+#             for ch in pattern:
+#                 if ch in need:
+#                     need[ch] += 1
+#                 else:
+#                     need[ch] = 1
+
+#             window = {}
+#             for ch in sub:
+#                 if ch in window:
+#                     window[ch] += 1
+#                 else:
+#                     window[ch] = 1
+
+#             ok = True
+#             for ch in need:
+#                 if ch not in window or window[ch] < need[ch]:
+#                     ok = False
+
+#             if ok:
+#                 if len(sub) < min_len:
+#                     min_len = len(sub)
+#                     ans = sub
+
+#     return ans
+# print(min_window ("ADOBECODEBANC", "ABC"))
+
+#second try
+# def min_window(log, pattern):
+#     ans = ""
+#     min_len = 10**9
+
+#     for i in range(len(log)):
+#         for j in range(i, len(log)):
+#             window = {}
+#             for k in range(i, j+1):
+#                 if log[k] in window:
+#                     window[log[k]] += 1
+#                 else:
+#                     window[log[k]] = 1
+
+#             need = {}
+#             for ch in pattern:
+#                 if ch in need:
+#                     need[ch] += 1
+#                 else:
+#                     need[ch] = 1
+
+#             match = True
+#             for ch in need:
+#                 if ch not in window or window[ch] < need[ch]:
+#                     match = False
+
+#             if match:
+#                 if j - i + 1 < min_len:
+#                     min_len = j - i + 1
+#                     ans = log[i:j+1]
+
+#     return ans
+# print(min_window ("ADOBECODEBANC", "ABC"))
+
+
+
 def min_window(log, pattern):
     if not log or not pattern:
         return ""
@@ -48,5 +120,6 @@ def min_window(log, pattern):
 print(min_window("ADOBECODEBANC", "ABC"))  
 print(min_window("a", "aa"))               
 print(min_window("a", "a"))               
+
 
 
